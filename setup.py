9ff98bc3c5fa5985ef46ee8792d9f2cd06a5c30c
Rajamohannaidu <rajamohan4urs@gmail.com>
2026-04-05 13:41:44 +0530

    cicd update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Rajamohan Naidu",
+    author_email="rajamohan4urs@gmail.com",
+    packages= find_packages(),
+    intstall_requires=[]
+)
